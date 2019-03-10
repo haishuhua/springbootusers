640428047dff4c0d1047e058464bf596a36b503f
Xiaoyi Chen <xchen@X-Man-MacBook-Pro.local>
2019-03-09 22:45:36 -0500

    add kafka

--- spring-boot-mysql-example-master/src/main/java/com/techprimers/db/SpringBootMysqldbApplication.java
+++ spring-boot-mysql-example-master/src/main/java/com/techprimers/db/SpringBootMysqldbApplication.java
@@ -1,5 +1,8 @@
 package com.techprimers.db;
 
+import com.techprimers.db.kafka.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,10 +11,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.techprimers.db.repository")
 @EnableCaching
 @SpringBootApplication
-public class SpringBootMysqldbApplication {
+public class SpringBootMysqldbApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMysqldbApplication.class, args);
+	}
+
+	@Autowired
+	private Sender sender;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		sender.send("Spring Kafka Producer and Consumer Example");
 	}
 
 
---
+++ spring-boot-mysql-example-master/src/main/java/com/techprimers/db/kafka/Receiver.java
@@ -0,0 +1,20 @@
+package com.techprimers.db.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Receiver {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Receiver.class);
+
+    @KafkaListener(topics = "${app.topic.foo}")
+    public void listen(@Payload String message) {
+        System.out.println(message);
+        LOG.info("received message='{}'", message);
+    }
+
+}
